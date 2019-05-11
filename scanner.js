e65c622e38d139c2fa2ff9b57cedc0eed624596c
Padna <coryf175@gmail.com>
2019-05-11 23:15:16 +0100

    added listings save functionality

--- scanner.js
+++ scanner.js
@@ -2,15 +2,19 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const URL_FORMAT = "https://www.ebay.com/sch/", FILE_FORMAT_LISTINGS = "./listings/";
+var PAGE_DEPTH = 5, ITEMS_PER_REQUEST = 200;
 
 console.log("scanner started...");
+fs.mkdir(FILE_FORMAT_LISTINGS, () => {});
 fs.readFile('./options.json', function callback(error, data) {
     if(error) {
         console.log("NOTICE: Can't read options.json, generating one for you... please restart with updated options.json.");
         console.log(error);
         let json = {
             time: 60,
-            keywords: ['keyword1', 'keyword2', 'keyword3']
+            keywords: ['keyword1', 'keyword2', 'keyword3'],
+            items_per_request: 200,
+            page_depth: 5
         };
         fs.writeFile('options.json', JSON.stringify(json), function callback(err) {
             if(err) {
@@ -21,6 +25,9 @@ fs.readFile('./options.json', function callback(error, data) {
     } else {
         let json = JSON.parse(data);
         let keywords = json.keywords, time = (json.time * 1000);
+        ITEMS_PER_REQUEST = json.items_per_request;
+        PAGE_DEPTH = json.page_depth;
+        console.log("Keywords: " + keywords + "\nIPR: " + ITEMS_PER_REQUEST + "\nPage Depth: " + PAGE_DEPTH);
         console.log("Performing first listing check... scheduled for every " + (time / 1000) + " minute(s).");
         keywords.forEach(function callback(i) {
             let newItems = saveListings(i);
@@ -37,27 +44,34 @@ fs.readFile('./options.json', function callback(error, data) {
 
 function saveListings(query) {
     let fileString = FILE_FORMAT_LISTINGS + query + ".json";
+    let all_links = [];
     fs.readFile(fileString, function callback(err, data) {
-        console.log("got ere");
-        request(URL_FORMAT + query, function callback(err, res, html) {
-            if(err || res.statusCode != 200) {
-                console.log("NOTICE: Failed to query eBay for listings on query " + query + ", stack will follow.");
-                console.log(err);
-            } else {
-                let $ = cheerio.load(html);
-                console.log("cheerio loaded html");
-            }
-        });
-        if(err) {
-        } else {
-            fs.unlink(fileString, function callback(err) {
-                if(err) {
-                    console.log("NOTICE: Failed to delete listings file " + fileString + ", stack will follow.");
-                    console.log(err);
-                    return null;
+        for(let i = 0; i <= PAGE_DEPTH-1; i++) {
+            console.log("SCRAPING PG " + (i+1) + " OF " + PAGE_DEPTH);
+            scrapeLinks(query, 1, ITEMS_PER_REQUEST, function callback(links) {
+                all_links = [].concat(all_links, links);
+                if((i+1) == PAGE_DEPTH) {
+                    if(err) {
+                        fs.writeFile(fileString, all_links, (err) => {
+                            if(err) {
+                                console.log("NOTICE: Failed to write links to file on query " + query + ", stack will follow.");
+                                console.log(err);
+                            } else {
+                                console.log("INFO: Writing " + all_links.length + " items to " + fileString);
+                            }
+                        });
+                    } else {
+                        fs.unlink(fileString, function callback(err) {
+                            if(err) {
+                                console.log("NOTICE: Failed to delete listings file " + fileString + ", stack will follow.");
+                                console.log(err);
+                                return null;
+                            }
+                        });
+                        
+                    }
                 }
             });
-            
         }
     });
 }
@@ -75,4 +89,18 @@ function saveData(fileName, jsonData) {
 
 function compare(data, data1) {
     return true;
+}
+
+function scrapeLinks(query, pageNumber, itemsPerRequest, callback) {
+    let links = [], queryLink = URL_FORMAT + query + "&ipg=" + itemsPerRequest + "&_pgn=" + pageNumber;
+    console.log(queryLink);
+    request(queryLink, (err, res, html) => {
+        if(err) console.log(err);
+        let $ = cheerio.load(html);
+        $('#srp-river-results').find('ul > li > div > div > a').each((index, element) => {
+            let link = $(element).attr('href');
+            links.push(link);
+        });
+        callback(links);
+    })
 }
